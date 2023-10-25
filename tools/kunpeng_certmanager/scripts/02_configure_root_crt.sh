767d992670a1d2f58281c4cbc5c1ceb6f7033a6c
hxy <xueyanghan@tencent.com>
2023-10-25 11:23:11 +0800

    02_configure_root_crt.sh update

--- tools/kunpeng_certmanager/scripts/02_configure_root_crt.sh
+++ tools/kunpeng_certmanager/scripts/02_configure_root_crt.sh
@@ -6,7 +6,7 @@
 ITRUSTEE_SDK_PATH="`pwd`/itrustee_sdk"
 
 # 1. 生成root证书
-openssl genrsa -out root.key 4096
+#openssl genrsa -out root.key 4096
 openssl req -new -x509 -key root.key -out root.crt -subj "/C=CN/L=F/O=testRootCA/OU=ACS/CN=Root CA" -days 3650
 
 #openssl x509 -in root.crt -pubkey -noout -C
@@ -14,6 +14,10 @@ openssl req -new -x509 -key root.key -out root.crt -subj "/C=CN/L=F/O=testRootCA
 # backup cert_config.h
 CertManagerPath="$ITRUSTEE_SDK_PATH/test/TA/cert_manager"
 cp $CertManagerPath/include/cert_config.h $CertManagerPath/include/cert_config.h_.`date +%Y%m%d%H%M%S`
+
+
+# compile renew_root_pub_tool
+go build -o renew_root_pub_tool ../renew_root_pub_tool/renew_root_pub_tool.go
 
 # renew root public key
 ./renew_root_pub_tool \
