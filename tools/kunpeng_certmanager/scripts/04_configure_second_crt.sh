24e9e2c0aba2bc16b9c82b0fbba8431c10fa9c5e
hxy <xueyanghan@tencent.com>
2023-10-26 10:29:06 +0800

    commend second generate sh, only run once

--- tools/kunpeng_certmanager/scripts/04_configure_second_crt.sh
+++ tools/kunpeng_certmanager/scripts/04_configure_second_crt.sh
@@ -5,17 +5,16 @@
 # 在itrustee_sdk目录下执行
 ITRUSTEE_SDK_PATH="`pwd`"
 
-cd $ITRUSTEE_SDK_PATH
-openssl genrsa -out second.key 4096
-openssl req -new -key second.key -out second.csr -subj "/C=CN/L=F/O=testRootCA/OU=ACS/CN=Secondary CA"
-
-openssl x509 -req -CA root.crt -CAkey root.key -CAcreateserial -in second.csr -out second.crt -sha256 -extfile openssl.cnf -extensions v3_ca -days 3650
-openssl x509 -in second.crt -outform der -out second.der
+# 仅执行一次，执行后，需要安全保存second.key、second.crt、second.der三个文件
+#cd $ITRUSTEE_SDK_PATH
+#openssl genrsa -out second.key 4096
+#openssl req -new -key second.key -out second.csr -subj "/C=CN/L=F/O=testRootCA/OU=ACS/CN=Secondary CA"
+#
+#openssl x509 -req -CA root.crt -CAkey root.key -CAcreateserial -in second.csr -out second.crt -sha256 -extfile openssl.cnf -extensions v3_ca -days 3650
+#openssl x509 -in second.crt -outform der -out second.der
 
 # 导入TA二级证书
 # 注：成功导入后，用户可基于二级证书自行颁发TA证书以及签发config，然后参与TA编译形成业务TA，
 # 此时安全OS仅允许加载三方TA（由用户二级证书签发的TA证书）以及系统TA（configs.xml注有<sys_verify_ta>标签）加载运行，
 # 原由华为签发的TA证书的TA将会加载失败。
 /usr/bin/certmanager import second.der
-
-rm -rf second.csr root.srl
